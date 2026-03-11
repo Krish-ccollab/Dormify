64ca08b5e8e49bdcf89b2ba3ac4f34bad27a2a97
Krish Modh <rmodh4@gmail.com>
2026-03-11 20:48:47 +0530

    css updta

--- rector-portal/src/pages/StudentDetails.jsx
+++ rector-portal/src/pages/StudentDetails.jsx
@@ -3,7 +3,7 @@
 import { useParams, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
-import "../styles/studentDetails.css"
+import "../styles/StudentDetails.css"
 import toast from "react-hot-toast";
 const API_URL = import.meta.env.VITE_API_URL;
 
