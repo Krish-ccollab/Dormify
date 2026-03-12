cd90a2331daef99d7140c25ad0e5874551e2a7c5
Krish Modh <rmodh4@gmail.com>
2026-03-12 17:37:57 +0530

    chanefe of otp

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -10,11 +10,13 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("EMAIL_USER"),
     MAIL_PASSWORD=os.getenv("EMAIL_PASSWORD"),
     MAIL_FROM=os.getenv("EMAIL_USER"),
+    MAIL_FROM_NAME="Dormify",
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
     MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
 
 async def send_otp(email: EmailStr, otp: str):
